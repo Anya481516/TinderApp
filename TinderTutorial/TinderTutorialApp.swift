8f4f4f11a0adbcffe84b57dfc95436c6dacdd2e0
Anna Melkhova <melxxova@gmail.com>
2024-12-03 19:55:15 +0100

    Initial Commit

---
+++ TinderTutorial/TinderTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTutorialApp.swift
+//  TinderTutorial
+//
+//  Created by Nikita Komarov on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
